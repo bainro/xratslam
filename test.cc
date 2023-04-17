6d607614292ca17b1b6bcfb6d7e79ab677cc73c9
Robert Bain <31903812+bainro@users.noreply.github.com>
2023-04-17 13:57:56 -0700

    Update test.cc

--- test.cc
+++ test.cc
@@ -2,12 +2,13 @@
 #include<xratslam/XRatSlam.hh>
 
 using namespace std;
+using namespace xratslam;
 
 int main()
 {
     cout << "Hello World!";
     string configFile = "";
-    XRatSlam slam = xratslam::XRatSlam("");
+    XRatSlam slam = XRatSlam("");
     slam::start();
     return 0;
 }
