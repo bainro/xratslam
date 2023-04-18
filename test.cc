7aa9db933ebecc81c6c370c8c79e1273f4d9e968
Robert Bain <31903812+bainro@users.noreply.github.com>
2023-04-17 22:59:05 -0700

    Update test.cc

--- test.cc
+++ test.cc
@@ -14,11 +14,14 @@ int main()
     XRatSlam slam(configFile);
     cout << slam.start() << endl;
     DataReaderVideo vid("./test.mp4");
+    cout << 123 << endl;
     vid.setOdometryVisual(configFile);
-    
+    cout << 234 << endl;
     Image **img;
     Odometry **odo;
     vid.readNext(img, odo);
+    cout << 345 << endl;
     cout << slam.feed(*img, *odo);
+    cout << 456 << endl;
     return 0;
 }
