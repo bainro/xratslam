041b5198e3dfe5a7c59b7b37c05cb2736b4fe8c5
Robert Bain <31903812+bainro@users.noreply.github.com>
2023-04-17 22:50:05 -0700

    Update test.cc

--- test.cc
+++ test.cc
@@ -1,5 +1,8 @@
 #include<iostream>
+#include<xratslam/Image.hh>
+#include<xratslam/Odometry.hh>
 #include<xratslam/XRatSlam.hh>
+#include<xratslam/DataReaderVideo.hh>
 
 using namespace std;
 using namespace xratslam;
@@ -10,5 +13,12 @@ int main()
     string configFile = "";
     XRatSlam slam(configFile);
     cout << slam.start() << endl;
+    DataReaderVideo vid("./test.mp4");
+    vid.setOdometryVisual(NULL);
+    
+    Image img;
+    Odometry odo;
+    vid.readNext(&img, &odo);
+    cout << slam.feed(&img, &odo);
     return 0;
 }
